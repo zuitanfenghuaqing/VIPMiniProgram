2603f939a030ea1c37415fe44305c3df1b3b3503
AmyStore <1137436729@qq.c0m>
2018-04-08 23:01:39 +0800

    swiper

---
+++ VIPMiniProgram/pages/productDetails/productDetails.js
@@ -0,0 +1,15 @@
+Page({
+    data: {
+        imgUrls: [
+          '../../images/ww.png',
+          '../../images/ww.png',
+          '../../images/ww.png',
+          '../../images/ww.png',
+          '../../images/ww.png'
+        ],
+        indicatorDots: true,
+        autoplay: false,
+        interval: 5000,
+        duration: 1000
+      },
+})
