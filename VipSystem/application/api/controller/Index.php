05d760dab64f37590de947f7010fe4a2bd4a9d97
unknown <1097422313@qq.com>
2018-04-14 11:53:42 +0800

    update .gitignore

--- VipSystem/application/api/controller/Index.php
+++ VipSystem/application/api/controller/Index.php
@@ -8,25 +8,27 @@ class Index
 {
     public function index()
     {
+        return json(['code'=>0,'message'=>'index']);
+        //test
         // $data = ['name'=>'thinkphp','url'=>'thinkphp.cn'];
         // 指定json数据输出
         // return json(['data'=>$data,'code'=>1,'message'=>'操作完成']);
-        $data = ['foo' => 'bar', 'bar' => 'foo'];
-        Db::name('demo')->insert($data);
-        $data = Db::name('demo')->find();
-        print_r($data);
+        // $data = ['foo' => 'bar', 'bar' => 'foo'];
+        // Db::name('demo')->insert($data);
+        // $data = Db::name('demo')->find();
+        // print_r($data);
 
-        $d = Db::name('demo')
-        ->field('id,name')
-        ->limit(100)->skip(1)
-        ->order('id', 'desc')
-        ->select();
-        print_r($d);
+        // $d = Db::name('demo')
+        // ->field('id,name')
+        // ->limit(100)->skip(1)
+        // ->order('id', 'desc')
+        // ->select();
+        // print_r($d);
     }
 
     public function test()
     {
-        echo 'test';
+        return json(['code'=>0,'message'=>'index']);
     }
     public function bar()
     {
